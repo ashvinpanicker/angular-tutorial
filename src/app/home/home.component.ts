fee1538badf83622189c8ec429b7cf917d4ba83f
Ashvin Panicker <agnjkafgh@gmail.com>
2025-03-21 19:10:04 +0530

    Chapter 4,ng gen interface and ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
-
+import { HousingLocation } from '../housingLocation';
+import locationsJson from '../../assets/housing-locations.json';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -14,11 +15,12 @@ import { HousingLocationComponent } from "../housing-location/housing-location.c
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
+      <app-housing-location *ngFor="let housingLocation of housingLocationList
+      " [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  housingLocationList: HousingLocation[] = locationsJson.locations;
 }
